8bc2606b30d01e5130647661e29bbfec7b8e1c0d
Jonathan Erthal <jonierthal@gmail.com>
2023-04-25 08:20:52 -0300

    Interromper ciclo

--- src/pages/Home/styles.ts
+++ src/pages/Home/styles.ts
@@ -86,7 +86,7 @@ export const Separator = styled.div`
   justify-content: center;
 `
 
-export const StartCountdownButoon = styled.button`
+export const BaseCountdownButoon = styled.button`
   width: 100%;
   border: 0;
   padding: 1rem;
@@ -99,17 +99,28 @@ export const StartCountdownButoon = styled.button`
   gap: 0.5rem;
   font-weight: bold;
 
-  cursor: pointer;
-
-  background: ${(props) => props.theme['green-500']};
   color: ${(props) => props.theme['gray-100']};
+
+  cursor: pointer;
 
   &:disabled {
     opacity: 0.7;
     cursor: not-allowed;
   }
+`
+
+export const StartCountdownButton = styled(BaseCountdownButoon)`
+  background: ${(props) => props.theme['green-500']};
 
   &:not(:disabled):hover {
     background: ${(props) => props.theme['green-700']};
+  }
+`
+
+export const StopCountdownButton = styled(BaseCountdownButoon)`
+  background: ${(props) => props.theme['red-500']};
+
+  &:not(:disabled):hover {
+    background: ${(props) => props.theme['red-700']};
   }
 `
