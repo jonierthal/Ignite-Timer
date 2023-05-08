c7677820a9fb1cd815ce3d2ba8ea586e66f67995
Jonathan Erthal <jonierthal@gmail.com>
2023-05-08 08:06:23 -0300

    Salvando um objeto no Reducer

--- src/contexts/CyclesContext.tsx
+++ src/contexts/CyclesContext.tsx
@@ -31,20 +31,49 @@ interface CyclesContextProviderProps {
   children: ReactNode
 }
 
+interface CyclesState {
+  cycles: Cycle[]
+  activeCycleId: string | null
+}
+
 export function CyclesContextProvider({
   children,
 }: CyclesContextProviderProps) {
-  const [cycles, dispatch] = useReducer((state: Cycle[], action: any) => {
-    if (action.type === 'ADD_NEW_CICLE') {
-      return [...state, action.payload.newCycle]
-    }
+  const [cyclesState, dispatch] = useReducer(
+    (state: CyclesState, action: any) => {
+      if (action.type === 'ADD_NEW_CICLE') {
+        return {
+          ...state,
+          cycles: [...state.cycles, action.payload.newCycle],
+          activeCycle: action.payload.newCycle.id,
+        }
+      }
 
-    return state
-  }, [])
+      if (action.type === 'INTERRUPT_CURRENT_CYCLE') {
+        return {
+          ...state,
+          cycles: state.cycles.map((cycle) => {
+            if (cycle.id === state.activeCycleId) {
+              return { ...cycle, finishedDate: new Date() }
+            } else {
+              return cycle
+            }
+          }),
+          activeCyclesId: null,
+        }
+      }
 
-  const [activeCycleId, setActiveCycleId] = useState<string | null>(null)
+      return state
+    },
+    {
+      cycles: [],
+      activeCycleId: null,
+    },
+  )
+
   const [amountSecondsPassed, setAmountSecondsPassed] = useState(0)
 
+  const { cycles, activeCycleId } = cyclesState
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId)
 
   function setSecondsPassed(seconds: number) {
@@ -58,16 +87,6 @@ export function CyclesContextProvider({
         activeCycleId,
       },
     })
-
-    //setCycles((state) =>
-      //state.map((cycle) => {
-        //if (cycle.id === activeCycleId) {
-          //return { ...cycle, finishedDate: new Date() }
-        //} else {
-          //return cycle
-        //}
-      //}),
-   // )
   }
 
   function createNewCycle(data: CreateCycleData) {
@@ -87,8 +106,7 @@ export function CyclesContextProvider({
       },
     })
 
-    //setCycles((state) => [...state, newCycle])
-    setActiveCycleId(id)
+    // setCycles((state) => [...state, newCycle])
     setAmountSecondsPassed(0)
   }
 
@@ -99,16 +117,15 @@ export function CyclesContextProvider({
         activeCycleId,
       },
     })
-    //setCycles((state) =>
-      //state.map((cycle) => {
-        //if (cycle.id === activeCycleId) {
-          //return { ...cycle, interrupdtedDate: new Date() }
-        //} else {
-          //return cycle
-        //}
-      //}),
-    //)
-    setActiveCycleId(null)
+    // setCycles((state) =>
+    //  state.map((cycle) => {
+    //  if (cycle.id === activeCycleId) {
+    //  return { ...cycle, interrupdtedDate: new Date() }
+    //  } else {
+    //  return cycle
+    //  }
+    //  }),
+    //  )
   }
 
   return (
